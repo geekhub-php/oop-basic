e0f0ad31d0352c3b8c8f581cacca08684f510215
Oleg Zinchenko <1cdecoder@gmail.com>
2012-10-17 19:54:42 +0300

    added basic files

---
+++ GeekHub/autoload.php
@@ -0,0 +1,12 @@
+<?php
+
+error_reporting(E_ALL);
+ini_set('display_errors', '1');
+
+require_once __DIR__ . '/Basic/BasicClass.php';
+require_once __DIR__ . '/Basic/BasicStaticClass.php';
+
+require_once __DIR__ . '/Basic/EatebleInterface.php';
+require_once __DIR__ . '/Basic/AbstractDish.php';
+require_once __DIR__ . '/Basic/PastaDish.php';
+
---
+++ GeekHub/index.php
@@ -0,0 +1,27 @@
+<?php
+
+require_once __DIR__ . '/autoload.php';
+
+use GeekHub\Basic\BasicClass;
+use GeekHub\Basic\BasicStaticClass;
+use GeekHub\Basic\PastaDish;
+
+$bc = new BasicClass();
+
+var_dump($bc);
+
+
+
+//var_dump(BasicStaticClass::PRIVACY_FRIENDS);
+//var_dump(BasicStaticClass::getPrivacySettings());
+
+
+//$pasta = new PastaDish('Italian');
+//
+//$pasta->eat();
+//$pasta->prepare();
+//$pasta->prepare();
+//$pasta->eat();
+
+
+
